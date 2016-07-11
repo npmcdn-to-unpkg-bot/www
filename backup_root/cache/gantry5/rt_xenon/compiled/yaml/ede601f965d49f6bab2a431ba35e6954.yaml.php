619d9fef55f0446f98dd0ccff7343f75acb1c153
NABC 2017 <abhi_neogi@yahoo.com>
2016-07-11 14:56:37 -0700

    removed backup_root

--- backup_root/cache/gantry5/rt_xenon/compiled/yaml/ede601f965d49f6bab2a431ba35e6954.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
-    'filename' => '/home/nabc/public_html/templates/rt_xenon/config/_error/page/body.yaml',
-    'modified' => 1453474406,
-    'data' => [
-        'doctype' => 'html'
-    ]
-];
